1a55fd372062d89b3a625827c2cf8b66bd7202ad
QY <1537946665@qq.com>
2023-05-09 16:13:50 +0800

    引入redis，未加日志

---
+++ bootstrap/redis.go
@@ -0,0 +1,18 @@
+package bootstrap
+
+import (
+	"fmt"
+	"goWeb/redis"
+)
+
+// SetupRedis 初始化 Redis
+func SetupRedis() {
+
+	// 建立 Redis 连接
+	redis.ConnectRedis(
+		fmt.Sprintf("%v:%v", "localhost", "6379"),
+		"",
+		"",
+		0,
+	)
+}
--- main.go
+++ main.go
@@ -8,7 +8,8 @@ import (
 
 func main() {
 	r := gin.New()
-	bootstrap.SetupDB()     //初始化数据库
+	bootstrap.SetupDB() //初始化数据库
+	bootstrap.SetupRedis()
 	bootstrap.SetupRoute(r) //初始化路由，包括中间件
 	err := r.Run(":3000")   //最好写到配置文件中端口（数据库连接，密钥......都不要写死）
 	if err != nil {
