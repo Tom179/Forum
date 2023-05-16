7a21e38e4a50dc095df29f173618d27786a61de8
QY <1537946665@qq.com>
2023-05-16 09:53:43 +0800

    GeustJwt

---
+++ app/http/MiddleWares/guest_jwt.go
@@ -0,0 +1,25 @@
+package MiddleWares
+
+import (
+	"goWeb/app/response"
+	"goWeb/jwt"
+
+	"github.com/gin-gonic/gin"
+)
+
+// GuestJWT 强制使用游客身份访问
+func GuestJWT() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if len(c.GetHeader("Authorization")) > 0 { //Authorization中有东西
+
+			_, err := jwt.NewJWT().ParserToken(c)
+			if err == nil { // 解析 token 成功，说明登录成功了,返回提示
+				response.Unauthorized(c, "请使用游客身份访问")
+				c.Abort()
+				return
+			}
+		}
+
+		c.Next()
+	}
+}
--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"goWeb/app/http/MiddleWares"
 	"goWeb/bootstrap"
+	"net/http"
 )
 
 func main() {
@@ -15,8 +17,11 @@ func main() {
 	/*r.GET("/test_auth", MiddleWares.AuthJWT(), func(c *gin.Context) { //中间件测试
 		userModel := auth.CurrentUser(c)
 		response.Data(c, userModel)
-	    中间件测试
+	    //中间件测试：登录身份，哪些路由需要授权才能访问，直接为其添加中间件即可
 	})*/
+	r.GET("/test_guest", MiddleWares.GuestJWT(), func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello guest")
+	}) //中间件测试：游客身份，哪些路由只有游客才能访问，直接为其添加中间件即可。
 	//fmt.Println(captcha.NewCaptcha().VerifyCaptcha("07d7ADH7Fn7cvV4Vf9nM", "743529")) //检测验证码函数
 
 	err := r.Run(":3000") //最好写到配置文件中端口（数据库连接，密钥......都不要写死）
